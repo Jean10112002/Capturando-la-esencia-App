e6eeaf5877326f0aeb743ffbf6a544855308beb7
Janblack07 <jandryjacob20@gmail.com>
2023-06-07 23:42:49 -0500

    Realizacion de perfil de usuario No Responsive

--- src/app/core/shared/materialComponents/Material.module.ts
+++ src/app/core/shared/materialComponents/Material.module.ts
@@ -5,18 +5,32 @@ import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatStepperModule} from '@angular/material/stepper';
 import {MatButtonModule} from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { MatGridListModule } from '@angular/material/grid-list';
+import {MatTabsModule} from '@angular/material/tabs';
+import { MatBadgeModule } from '@angular/material/badge';
 
 @NgModule({
   imports: [MatButtonModule,
     MatDialogModule,
     MatInputModule,
     MatFormFieldModule,
-    MatStepperModule],
+    MatIconModule ,
+    MatCardModule,
+    MatTabsModule,
+    MatGridListModule,
+    MatStepperModule,
+    MatBadgeModule],
   exports: [MatButtonModule,
     MatDialogModule,
     MatInputModule,
+    MatIconModule,
+    MatGridListModule,MatCardModule,
     MatFormFieldModule,
-    MatStepperModule],
+    MatStepperModule,
+    MatTabsModule,
+    MatBadgeModule,],
   declarations: [],
   providers: [],
 })
--- src/app/private/pages/profile/container/profile.component.ts
+++ src/app/private/pages/profile/container/profile.component.ts
@@ -1,3 +1,4 @@
+import { Comentario } from './../../../interfaces/post/post.search-by-categoria.interface';
 import { Component, OnInit } from '@angular/core';
 import { UserInformationService } from 'src/app/private/services/user-information.service';
 
@@ -15,5 +16,15 @@ export class ProfileComponent implements OnInit {
       console.log(data)
     })
   }
+  gallery = [
+    { url: 'https://img2.wallspic.com/previews/4/9/5/2/5/152594/152594-lisa_simpson-los_simpsons-simpsons_telefono_caso-bart_simpson-homer_simpson-550x310.jpg', alt: 'Liza Cool' ,like:'50',comentario:'100' },
+    { url: 'https://img2.wallspic.com/previews/4/9/5/2/5/152594/152594-lisa_simpson-los_simpsons-simpsons_telefono_caso-bart_simpson-homer_simpson-550x310.jpg', alt: 'Liza Cool' ,like:'10',comentario:'10' },
+    { url: 'https://img2.wallspic.com/previews/4/9/5/2/5/152594/152594-lisa_simpson-los_simpsons-simpsons_telefono_caso-bart_simpson-homer_simpson-550x310.jpg', alt: 'Liza Cool' ,like:'20',comentario:'20' },
+    { url: 'https://img2.wallspic.com/previews/4/9/5/2/5/152594/152594-lisa_simpson-los_simpsons-simpsons_telefono_caso-bart_simpson-homer_simpson-550x310.jpg', alt: 'Liza Cool' ,like:'30',comentario:'30' },
+    { url: 'https://img2.wallspic.com/previews/4/9/5/2/5/152594/152594-lisa_simpson-los_simpsons-simpsons_telefono_caso-bart_simpson-homer_simpson-550x310.jpg', alt: 'Liza Cool' ,like:'40',comentario:'40' },
+    { url: 'https://img2.wallspic.com/previews/4/9/5/2/5/152594/152594-lisa_simpson-los_simpsons-simpsons_telefono_caso-bart_simpson-homer_simpson-550x310.jpg', alt: 'Liza Cool' ,like:'10',comentario:'50' },
+
+    // Agrega más imágenes aquí
+  ];
 
 }
