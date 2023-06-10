af30f954d688bc13ca135c5849fa97e4ac4bfdda
Jean Rodriguez <jeanpierrerodriguezzambrano@gmail.com>
2023-06-10 11:22:49 -0500

    agregado components para modal y cada comentario

---
+++ src/app/private/pages/home/components/list-user-comment/list-user-comment.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-user-comment',
+  templateUrl: './list-user-comment.component.html',
+  styleUrls: ['./list-user-comment.component.scss']
+})
+export class ListUserCommentComponent {
+
+}
---
+++ src/app/private/pages/home/components/modal-user-comments/modal-user-comments.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-modal-user-comments',
+  templateUrl: './modal-user-comments.component.html',
+  styleUrls: ['./modal-user-comments.component.scss']
+})
+export class ModalUserCommentsComponent {
+
+}
