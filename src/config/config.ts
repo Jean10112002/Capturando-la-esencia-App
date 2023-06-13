7a1a56f69e021c7ba4a220f778e4b5d6b5c282be
Jean Rodriguez <jeanpierrerodriguezzambrano@gmail.com>
2023-06-12 22:21:46 -0500

    solucionado pequeños errores

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,11 +1,8 @@
 import { environment } from "src/environments/environment";
 
 export const config={
-
-   apiUrl:environment.apiUrl,
-
-
-
+  /*  apiUrl:"http://127.0.0.1:8000/api/", */
+    apiUrl:environment.apiUrl,
   avatarUrl:environment.avatarUrl,
 
 
--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   avatarUrl:'https://res.cloudinary.com/darohu37k/image/upload/v1686431916/Minimizar%20img/Avatar_1_1_jrtrbu.png',
-  apiUrl: 'http://127.0.0.1:8000/api/'
+  apiUrl: 'https://api-ambiente-saludable-production.up.railway.app/api/'
 };
