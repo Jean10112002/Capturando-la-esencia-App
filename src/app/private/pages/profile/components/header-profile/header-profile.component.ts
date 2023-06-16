9df164d203ab554e4533247674f3a0ac24df1ae0
Keyron13 <aguilarortizkeirodamian@gmail.com>
2023-06-16 14:33:04 -0500

    user post

--- src/app/private/pages/profile/components/header-profile/header-profile.component.ts
+++ src/app/private/pages/profile/components/header-profile/header-profile.component.ts
@@ -1,6 +1,8 @@
 import { Component, Input } from '@angular/core';
 import { Participante } from 'src/app/private/interfaces/participante/participante.interface';
 import { config } from 'src/config/config';
+import { ModalPostComponent } from '../modal-post/modal-post.component';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-header-profile',
@@ -10,4 +12,11 @@ import { config } from 'src/config/config';
 export class HeaderProfileComponent {
   @Input() user!:Participante;
   avatar:string=config.avatarUrl;
+
+  constructor(public dialog: MatDialog) {}
+
+  openDialog() {
+    this.dialog.open(ModalPostComponent,{width:'80%'});
+  }
+
 }
