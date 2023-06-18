7dd5db5b82fb7d16696539835397c40fc9a3c4ac
Janblack07 <jandryjacob20@gmail.com>
2023-06-18 17:53:12 -0500

    Se realizo la validacion del calificar

--- src/app/private/pages/home/components/calificar-post/calificar-post.component.ts
+++ src/app/private/pages/home/components/calificar-post/calificar-post.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-calificar-post',
@@ -6,6 +7,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./calificar-post.component.scss'],
 })
 export class CalificarPostComponent {
+  myForm!: FormGroup;
+  constructor(private formBuilder: FormBuilder) {}
+  ngOnInit() {
+    this.buildForm();
+  }
 
+  buildForm() {
+    this.myForm = this.formBuilder.group({
+      contenido: ['', [Validators.required]],
+      organizacion: ['', Validators.required],
+      creatividad: ['', [Validators.required]],
+      tecnica: ['', Validators.required],
+    });
+  }
+  onSubmit(form:any) {
+    if (this.myForm.invalid) {
+      // Marcar los campos del formulario como tocados para mostrar los mensajes de error
+      Object.values(this.myForm.controls).forEach((control) =>
+        control.markAsTouched()
+      );
+      return;
+    } else {
 
-}
+      };
+    }
+  }
