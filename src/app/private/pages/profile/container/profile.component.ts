c0457f5b9fa215f5da8a6b6b7d8d8daf7304be08
Keyron13 <aguilarortizkeirodamian@gmail.com>
2023-06-21 00:50:36 -0500

    navbar en categoria profile

--- src/app/private/pages/profile/container/profile.component.ts
+++ src/app/private/pages/profile/container/profile.component.ts
@@ -6,10 +6,12 @@ import { UserInformationService } from 'src/app/private/services/user-informatio
 import { ActivatedRoute, Route, Router } from '@angular/router';
 import { Observable, Subject, takeUntil } from 'rxjs';
 import { SpinnerService } from 'src/app/core/shared/services/spinner.service';
-import { ParticipanteShowI } from 'src/app/private/interfaces/participante/participante.interface';
+import { Participante, ParticipanteShowI } from 'src/app/private/interfaces/participante/participante.interface';
 import { ParticipanteService } from 'src/app/private/services/participante.service';
 import { EnventEmissorService } from 'src/app/private/services/envent-emissor.service';
 import { eventEmissorI } from 'src/app/private/interfaces/event-emissor/event-emissor.interface';
+import { UserI, UserProfileI } from 'src/app/public/interfaces/Login.response.interface';
+import { AuthService } from 'src/app/public/services/auth.service';
 
 @Component({
   selector: 'app-profile',
@@ -18,17 +20,35 @@ import { eventEmissorI } from 'src/app/private/interfaces/event-emissor/event-em
 })
 export class ProfileComponent implements OnInit,OnDestroy {
   userInformation$!: Observable<ParticipanteShowI>;
+
+  user!: UserI | Participante;
+
   id!:number
   participante!:any
   private destroy$: Subject<void> = new Subject<void>();
 
   constructor(
     private route: ActivatedRoute,
+    private readonly authService: AuthService,
     private readonly participanteService: ParticipanteService,
       participanteServiceData:UserInformationService,
     private readonly router:Router,
     eventEmissorService:EnventEmissorService
   ) {
+
+    authService.userInformation().subscribe((user: UserProfileI) => {
+      this.user = user.user;
+      if (this.user.rol === 'admin') {
+        participanteServiceData.setInformationUser(user.user);
+      }
+      if (this.user.rol === 'jurado') {
+        participanteServiceData.setInformationUser(user.user);
+      }
+      if (this.user.rol === 'participante') {
+        participanteServiceData.setInformationParticipante(user.user);
+      }
+    });
+
     participanteServiceData.getInformationParticipante().pipe(takeUntil(this.destroy$)).subscribe((participante) => {
       const rol=participante.rol;
       participanteServiceData.getData().pipe(takeUntil(this.destroy$)).subscribe(data=>{
