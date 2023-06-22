dda8d0586b3f47beda7bb308ed8eb78b098b8cec
Janblack07 <jandryjacob20@gmail.com>
2023-06-21 19:07:46 -0500

    Se realizo estilo en las tablas de reporte

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,7 +1,7 @@
 import { environment } from "src/environments/environment.development";
 
 export const config={
-    /* apiUrl:environment.apiUrl, */
-   apiUrl:'http://localhost:8000/api/',
+     apiUrl:environment.apiUrl,
+  /*  apiUrl:'http://localhost:8000/api/', */
   avatarUrl:environment.avatarUrl,
 }
