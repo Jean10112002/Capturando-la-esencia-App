b79ae1073ff85642fa4b3da24010d2f0872256db
Janblack07 <jandryjacob20@gmail.com>
2023-06-21 20:11:29 -0500

    Se realizo cambios en el estilo

--- src/config/config.ts
+++ src/config/config.ts
@@ -2,6 +2,6 @@ import { environment } from "src/environments/environment.development";
 
 export const config={
      apiUrl:environment.apiUrl,
-  /*  apiUrl:'http://localhost:8000/api/', */
+/*   apiUrl:'http://localhost:8000/api/', */
   avatarUrl:environment.avatarUrl,
 }
