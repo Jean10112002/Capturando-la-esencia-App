bb61ccf9ff0ad615405573bba9f5b77cc99a56dd
Keyron13 <aguilarortizkeirodamian@gmail.com>
2023-07-05 10:39:42 -0500

    Actualizacion de la url

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   avatarUrl:'https://res.cloudinary.com/darohu37k/image/upload/v1686431916/Minimizar%20img/Avatar_1_1_jrtrbu.png',
-  apiUrl: 'https://api-ambiente-saludable-production.up.railway.app/api/'
+  apiUrl: 'http://35.173.213.64/api/'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://api-ambiente-saludable-production.up.railway.app/api/',
+  apiUrl: 'http://35.173.213.64/api/',
   avatarUrl:'https://res.cloudinary.com/darohu37k/image/upload/v1686431916/Minimizar%20img/Avatar_1_1_jrtrbu.png'
 };
