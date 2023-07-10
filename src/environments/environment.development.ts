b5a4a9f807cd75407f200af2d83aa57ded85aacd
Jean Rodriguez <jeanpierrerodriguezzambrano@gmail.com>
2023-07-09 19:44:36 -0500

    scroll infinito solucionado

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   avatarUrl:'https://res.cloudinary.com/darohu37k/image/upload/v1686431916/Minimizar%20img/Avatar_1_1_jrtrbu.png',
-  apiUrl: 'https://api-ambiente-saludable-production.up.railway.app/api/'
+  apiUrl: 'https://mb-production.up.railway.app/api/'
 };
